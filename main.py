81f795f10eb25b671840f3963f27cd42d8762571
jul1998 <jguevara321@gmail.com>
2023-04-05 17:04:47 -0600

    export

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ BOT_ID = client.api_call("auth.test")["user_id"]
 
 
 members_waiting_Q = deque([])
+members_with_cases_Q = deque([])
 lunch_Q = deque()
 other_tasks_Q = deque()
 
@@ -38,6 +39,7 @@ hours = {
     17: ["Julian", "Julian Andres", "Jonathan", "Tony"],
     18: ["Julian", "Julian Andres", "Jonathan", "Tony"],
     19: ["Julian", "Julian Andres", "Jonathan", "Tony", "arr"], }
+
 
 
 
@@ -85,9 +87,10 @@ def execute_command(command_name, user_id):
         return True
     return False
 def display_text_list():
-    list_text = f"[Members waiting for cases at {get_current_hour()} hours]\n" + "\n".join([f"* {name}" for name in members_waiting_Q])
-    list_text += "\n[Members in lunch]\n" + "\n".join([f"* {name}" for name in lunch_Q])
-    list_text += "\n[Members with other tasks]\n" + "\n".join([f"* {name}" for name in other_tasks_Q])
+    list_text = f"[Members waiting for cases]\n" + "\n".join([f"{name}" for name in members_waiting_Q])
+    list_text += "\n[Members with cases]\n" + "\n".join([f"{name}" for name in members_with_cases_Q])
+    list_text += "\n[Members in lunch]\n" + "\n".join([f"{name}" for name in lunch_Q])
+    list_text += "\n[Members with other tasks]\n" + "\n".join([f"{name}" for name in other_tasks_Q])
     return list_text
 
 def add_to_lunch_queue(username, channel_id):
@@ -96,6 +99,9 @@ def add_to_lunch_queue(username, channel_id):
         members_waiting_Q.remove(username)
     elif username in other_tasks_Q:
         other_tasks_Q.remove(username)
+        lunch_Q.append(username)
+    elif username in members_with_cases_Q:
+        members_with_cases_Q.remove(username)
         lunch_Q.append(username)
     elif username in lunch_Q:
         client.chat_postMessage(channel=channel_id, text=f"{username} is already in the lunch queue")
@@ -109,12 +115,16 @@ def add_member_to_waiting_queue(channel_id, username):
         client.chat_postMessage(channel=channel_id, text=f"{username} is already in the waiting queue")
     elif username in lunch_Q:
         lunch_Q.remove(username)
-        members_waiting_Q.appendleft(username)
+        members_waiting_Q.append(username)
         client.chat_postMessage(channel=channel_id, text=f"{username} has been moved from the lunch queue to the waiting queue")
     elif username in other_tasks_Q:
         other_tasks_Q.remove(username)
-        members_waiting_Q.appendleft(username)
+        members_waiting_Q.append(username)
         client.chat_postMessage(channel=channel_id, text=f"{username} has been moved from other tasks to the waiting queue")
+    elif username in members_with_cases_Q:
+        members_with_cases_Q.remove(username)
+        members_waiting_Q.append(username)
+        client.chat_postMessage(channel=channel_id, text=f"{username} has been moved from the cases queue to the waiting queue")
     else:
         members_waiting_Q.append(username)
         client.chat_postMessage(channel=channel_id, text=f"{username} has been added to the waiting queue")
@@ -129,6 +139,9 @@ def add_member_to_other_tasks_queue(channel_id, username):
     elif username in lunch_Q:
         lunch_Q.remove(username)
         other_tasks_Q.appendleft(username)
+    elif username in members_with_cases_Q:
+        members_with_cases_Q.remove(username)
+        other_tasks_Q.appendleft(username)
     else:
         other_tasks_Q.appendleft(username)
     client.chat_postMessage(channel=channel_id, text=display_text_list())
@@ -136,11 +149,13 @@ def add_member_to_other_tasks_queue(channel_id, username):
 def assign_case(username,channel_id):
     if username in members_waiting_Q:
         members_waiting_Q.remove(username)
-        members_waiting_Q.append(username)
+        members_with_cases_Q.append(username)
     elif username in other_tasks_Q:
         other_tasks_Q.remove(username)
     elif username in lunch_Q:
         lunch_Q.remove(username)
+    else:
+        members_with_cases_Q.append(username)
     client.chat_postMessage(channel=channel_id, text=display_text_list())
 
 def exit_from_main_list(username, channel_id):
@@ -160,15 +175,56 @@ def remove_user_from_queue(user_name, channel_id, user_to_removed):
         other_tasks_Q.remove(user_to_removed)
     elif user_to_removed in lunch_Q:
         lunch_Q.remove(user_to_removed)
-        client.chat_postMessage(channel=channel_id, text=f"{user_to_removed} has been removed from all the queues by {user_name}")
-        client.chat_postMessage(channel=channel_id, text=display_text_list())
+
     else:
         client.chat_postMessage(channel=channel_id, text=f"{user_to_removed} is in no queue any longer")
+        client.chat_postMessage(channel=channel_id, text=display_text_list())
+    client.chat_postMessage(channel=channel_id, text=f"{user_to_removed} has been removed from all the queues by {user_name}")
+    client.chat_postMessage(channel=channel_id, text=display_text_list())
+
+def add_to_top(username, channel_id):
+    if username in members_waiting_Q:
+        members_waiting_Q.remove(username)
+        members_waiting_Q.appendleft(username)
+    elif username in lunch_Q:
+        lunch_Q.remove(username)
+        members_waiting_Q.appendleft(username)
+        client.chat_postMessage(channel=channel_id, text=f"{username} has been moved from the lunch queue to the top of the waiting queue")
+    elif username in other_tasks_Q:
+        other_tasks_Q.remove(username)
+        members_waiting_Q.appendleft(username)
+        client.chat_postMessage(channel=channel_id, text=f"{username} has been moved from other tasks to the top of the waiting queue")
+    else:
+        members_waiting_Q.appendleft(username)
+        client.chat_postMessage(channel=channel_id, text=f"{username} has been added to the top of the waiting queue")
+    client.chat_postMessage(channel=channel_id, text=display_text_list())
+
+def add_multiple_users_to_specific_queue(queue,user_who_moved, channel_id, user_names):
+    print("Channeld id in function",channel_id)
+    for user_name in user_names:
+        if queue == "waiting":
+            add_member_to_waiting_queue(channel_id, user_name)
+        elif queue == "lunch":
+            add_to_lunch_queue(user_name, channel_id)
+        elif queue == "other":
+            add_member_to_other_tasks_queue(channel_id, user_name)
+        elif queue == "top":
+            add_to_top(user_name, channel_id)
+        elif queue == "remove":
+            remove_user_from_queue(user_who_moved, channel_id, user_name)
+        elif queue == "with_case":
+            assign_case(user_name, channel_id)
+    return
+
+
 
 
 @app.route("/")
 def hello():
     return "Hello World!aa"
+
+last_command_time = {}
+
 @slack_event_adapter.on("message")
 def message(payload):
     event = payload.get("event", {})
@@ -179,11 +235,19 @@ def message(payload):
         # Get the user info using the users_info API method
         user_info = client.users_info(user=user_id)
         username = user_info["user"]["profile"]["real_name"]
+
         if execute_command(text, user_id):
             client.chat_postMessage(channel=channel_id, text="Wait 5 seconds before using this command again")
             return
         if text.lower() == "list":
             client.chat_postMessage(channel=channel_id, text=display_text_list())
+        elif text.lower() == "lunch timer":
+            if username not in lunch_Q:
+                add_to_lunch_queue(username, channel_id)
+            if username in lunch_Q:
+                time.sleep(20)
+                add_member_to_waiting_queue(channel_id, username)
+                return
         elif text.lower() == "lunch":
             add_to_lunch_queue(username, channel_id)
         elif text.lower() == "ready":
@@ -194,6 +258,8 @@ def message(payload):
             add_member_to_other_tasks_queue(channel_id, username)
         elif text.lower() == "done":
             assign_case(username,channel_id)
+        elif text.lower() == "top":
+            add_to_top(username,channel_id)
         elif text.lower() == "eos":
             exit_from_main_list(username, channel_id)
         else:
@@ -209,13 +275,16 @@ def slack_events():
     list_of_commands = "List of commands:\n" \
                        "list: List all members waiting for cases\n" \
                        "lunch: Add yourself to the lunch queue\n" \
+                       "lunch timer: Add yourself to the lunch queue and return to waiting Q after 1 hour\n" \
                        "ready: Add yourself to the waiting queue\n" \
                        "eos: End of your shift\n" \
                        "done: Say you have assigned your cases in MagnumPi\n" \
                        "back: Add yourself to the waiting queue\n" \
                        "other: Add yourself to other tasks queue\n" \
-                       "/remove_user: Remove any user from the all the Qs\n" \
+                       "top: Add yourself to the top of the waiting queue\n" \
+                        "/remove_user: Remove any user from the all the Qs\n" \
                        "/export dd-mm-yyyy dd-mm-yyyy: Export messages within date range\n" \
+                       "/add_members_to_specific_queue queue name, *user names in slack separated by comma*: Export messages within date range\n" \
                        "/help: List all commands\n"
     client.chat_postMessage(channel=channel_id, text=list_of_commands)
     return Response(), 200
@@ -227,11 +296,11 @@ def export_data_from_channel():
 
     # Get the channel ID from the request
     channel_id = request.form['channel_id']
+    print(channel_id)
 
 
     # Split the text of the command into two date strings
     start_date_str, end_date_str = text.split()
-    print(start_date_str, end_date_str)
 
     # Parse the date strings into datetime objects
     try:
@@ -256,7 +325,7 @@ def export_data_from_channel():
             timeout=50
         )
         messages = result['messages']
-        #print(messages)
+        print(result)
 
     except SlackApiError as e:
         client.chat_postMessage(channel=channel_id, text="Error: " + str(e) + " Please try again")
@@ -269,6 +338,7 @@ def export_data_from_channel():
             user_id = message['user']
             user_info = client.users_info(user=user_id)
             user_name = user_info['user']['real_name']
+            print(message)
         else:
             user_name = 'Unknown'
 
@@ -299,15 +369,6 @@ def export_data_from_channel():
     print("here1")
     files = client.files_list(user=BOT_ID)
     print(files)
-    upload_and_then_share_file = client.files_upload_v2(
-        channel=channel_id,  # You can specify multiple channels here in the form of a string array
-        title="Test text data",
-        filename="test.txt",
-        content="Hi there! This is a text file!",
-        initial_comment="Here is the file:",
-    )
-    file_id = upload_and_then_share_file['file']['id']
-    print("here")
     return Response(), 200
 
 @app.route("/remove_user", methods=['POST'])
@@ -316,27 +377,36 @@ def remove_user_from_Q():
     channel_id = data.get("channel_id")
     user_id = data.get("user_id")
     user_to_removed = data['text']
-    #print(user_to_removed)
-    #get if user is admin or not in workspace
     user_info = client.users_info(user=user_id)
-    #new_user = client.users_info(user=user_to_removed)
-    #print(new_user)
     user_name = user_info['user']['real_name']
-    if user_info['user']['is_admin'] == True:
 
-        remove_user_from_queue(user_name, channel_id, user_to_removed)
-    else:
-        client.chat_postMessage(channel=channel_id, text=f"{user_name} tried to remove {user_to_removed}, but is not "
-                                                         f"an admin")
+    remove_user_from_queue(user_name, channel_id, user_to_removed)
     return Response(), 200
 
+@app.route("/add_members_to_specific_queue", methods=['POST'])
+def add_members_to_specific_queue():
+    data = request.form
+    channel_id = data.get("channel_id")
+    user_id = data.get("user_id")
+    user_info = client.users_info(user=user_id)
+    user_who_moved = user_info['user']['real_name']
+    text = data['text']
+    raw_list = text.split(",")
+    queue = raw_list.pop(0)
+    print(raw_list)
+    users_to_move = [x.strip() for x in raw_list]
+    print(users_to_move)
+    client.chat_postMessage(channel=channel_id, text=f"Text {text}, user: {user_who_moved}, queue: {queue} channel: {channel_id}")
+    try:
+        add_multiple_users_to_specific_queue(queue, user_who_moved, channel_id, users_to_move)
+    except Exception as e:
+        client.chat_postMessage(channel=channel_id, text="Error: " + str(e) + " Please try again")
+        return f"Error: {e}"
+    return Response(), 200
 
 @app.route('/')
 def index():
     return ("Here asdasd")
 
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
     app.run(debug=True)
